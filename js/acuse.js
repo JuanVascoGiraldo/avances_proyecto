aaa3362e63e17b7aa337b3439846bdda044f68a5
JuanVascoGiraldo <79484258+JuanVascoGiraldo@users.noreply.github.com>
2024-11-24 12:55:32 -0600

    fix sweet alert icon error

--- js/acuse.js
+++ js/acuse.js
@@ -48,7 +48,7 @@ function validar_formulario() {
             html:`
                 <p>Descarga tu acuse de recibo</p>
                 <p>Da click en el boton para descargar, recuerda seguir los pasos que indica el archivo para que puedas continuar con tu registro</p>
-                <a href="../public/avanceProyectoNov24.pdf" download="Acuse.pdf" class="btn btn-outline-success btn-block">
+                <a href="../pdfs/avanceProyectoNov24.pdf" download="Acuse.pdf" class="btn btn-outline-success btn-block">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-download-fill" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 0a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 4.095 0 5.555 0 7.318 0 9.366 1.708 11 3.781 11H7.5V5.5a.5.5 0 0 1 1 0V11h4.188C14.502 11 16 9.57 16 7.773c0-1.636-1.242-2.969-2.834-3.194C12.923 1.999 10.69 0 8 0m-.354 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V11h-1v3.293l-2.146-2.147a.5.5 0 0 0-.708.708z"></path></svg>
                 Descargar acuse
